70044fcd1a8925e880f0ce2267f997c839b9f3be
gurung69 <gurungkrish44@gmail.com>
2022-09-16 11:50:21 +0545

    add ui to rps

--- script.js
+++ script.js
@@ -2,61 +2,41 @@
 let computerWin = 0;
 let playerWin = 0;
 
-/*
-create a function getComputerChoice
-  declare array of rock paper scissors
-  decleare a integer variable which stores random number 0-2
-  use the random number as array index to get the computer choice 
-*/
-
-function getComputerChoice(){
+function getComputerChoice(){//get the computer selection
     const choice = ['Rock', 'Paper', 'Scissors'];
-    const randomNum = Math.floor(Math.random()*3);
+    const randomNum = Math.floor(Math.random()*3);//picks number from 0-2
     return choice[randomNum];
 }
 
-/*
-create a function playRound with parameter that takes player input and computer choice
-  call function to get computer choice
-  compare user input and computer choice by turning both into upper case
-  return the result
-*/
-
-function playRound(playerSelection, computerSelection){
-    if (playerSelection.toUpperCase() === computerSelection.toUpperCase()){
+function playRound(e){
+    const playerSelection = e.target.value.toUpperCase();
+    const computerSelection = getComputerChoice().toUpperCase();
+    if (playerSelection === computerSelection.toUpperCase()){
         return `Draw both used ${computerSelection}`;
     }
-    else if (playerSelection.toUpperCase() === 'ROCK' && computerSelection.toUpperCase()=='PAPER' 
-    || playerSelection.toUpperCase()==='PAPER' && computerSelection.toUpperCase()=='SCISSORS' 
-    || playerSelection.toUpperCase()==='SCISSORS' && computerSelection.toUpperCase()==='ROCK'){
+    else if (playerSelection === 'ROCK' && computerSelection =='PAPER' 
+    || playerSelection==='PAPER' && computerSelection =='SCISSORS' 
+    || playerSelection ==='SCISSORS' && computerSelection ==='ROCK'){
         computerWin++;
-        return `You lose! ${computerSelection} beats ${playerSelection} `;
+        displayResult.textContent = `You lose! ${computerSelection} beats ${playerSelection}`
     }
     else{
         playerWin++;
-        return `You win! ${playerSelection} beats ${computerSelection}`;
+        displayResult.textContent = `You win! ${playerSelection} beats ${computerSelection}`
+    }
+    if (computerWin + playerWin >= 5){
+        displayResult.innerHTML = `<div class='score'>Your Win: ${playerWin} Computer Win: ${computerWin}</div>`
+        if(computerWin > playerWin){
+            displayResult.innerHTML += `<div class = winner>Computer Win</div>`
+        }
+        else{
+            displayResult.innerHTML += `<div class = winner>You Win</div>`
+        }
+        [computerWin, playerWin] = [0,0];
     }
 }
 
-/*
-create function game
-  ask for user input
-  pass the user input and computer to playRound and call it for 5 times
-*/
+const selections = document.querySelectorAll('.selection');
+const displayResult = document.querySelector('.result');
 
-function game(){
-    for(let i=0; i<5; i++){
-        const playerSelection = prompt('Enter Rock, Paper or Scissors: ')
-        const computerSelection = getComputerChoice()
-        console.log(playRound(playerSelection, computerSelection))
-    }
-    let winner;
-    if (computerWin == playerWin){
-        console.log('Draw')
-    }
-    else{
-        winner = playerWin > computerWin? 'You' : 'Computer';
-        console.log(`Winner: ${winner}`)
-    }
-}
-game()
+selections.forEach(selection => selection.addEventListener('click', playRound))
