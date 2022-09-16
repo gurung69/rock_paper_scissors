9193242a25567d5b82c387d19c3d774f8df77f4d
gurung69 <gurungkrish44@gmail.com>
2022-09-16 18:13:13 +0545

    add playgain feature after 5 win by player or com

--- script.js
+++ script.js
@@ -8,13 +8,8 @@ function getComputerChoice(){//get the computer selection
     return choice[randomNum];
 }
 
-function playRound(e){//rps is played whenever user click on the options
-    const playerSelection = e.target.value.toUpperCase();
-    const computerSelection = getComputerChoice().toUpperCase();
-    if (playerSelection === computerSelection.toUpperCase()){
-        return `Draw both used ${computerSelection}`;
-    }
-    else if (playerSelection === 'ROCK' && computerSelection =='PAPER' 
+function getWinner(playerSelection, computerSelection){//increment the score accoriding to who wins
+    if (playerSelection === 'ROCK' && computerSelection =='PAPER' 
     || playerSelection==='PAPER' && computerSelection =='SCISSORS' 
     || playerSelection ==='SCISSORS' && computerSelection ==='ROCK'){
         computerWin++;
@@ -22,9 +17,20 @@ function playRound(e){//rps is played whenever user click on the options
     else{
         playerWin++;
     }
+    displayPoint();
+}
+
+function displayPoint(){//display points of playerWIn and ComputerWin
     playerPoint.textContent = playerWin;
     computerPoint.textContent = computerWin;
+}
+
+function playRound(e){//rps is played whenever user click on the options
+    const playerSelection = e.target.value.toUpperCase();
+    const computerSelection = getComputerChoice().toUpperCase();
+    getWinner(playerSelection, computerSelection);
     if (computerWin >= 5 || playerWin >= 5){//checks the winner if computer or player wins 5 round
+        toogleHidden();
         if(computerWin > playerWin){
             displayWinner.innerHTML = `Computer Wins`;
         }
@@ -35,9 +41,24 @@ function playRound(e){//rps is played whenever user click on the options
     }
 }
 
+function toogleHidden(){//toggle hidden class
+    selectionBox.classList.toggle('hidden');
+    playAgain.classList.toggle('hidden');
+}
+
+function newRound(){//toogle hidden class and set points back to 0 0
+    toogleHidden();
+    displayPoint();
+}
+
+const selectionBox = document.querySelector('.selections')
 const selections = document.querySelectorAll('.selection');
 const playerPoint = document.querySelector('.player-point');
 const computerPoint = document.querySelector('.computer-point');
 const displayWinner = document.querySelector('.winner');
+const playAgain = document.querySelector('.play-again');
+const newGame = document.querySelector('.new-game');
+const gameFinished = false;
 
-selections.forEach(selection => selection.addEventListener('click', playRound))
+selections.forEach(selection => selection.addEventListener('click', playRound));
+newGame.addEventListener('click', newRound);//starts new round of rps
